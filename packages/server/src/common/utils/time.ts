7b0ac21842d1723fbeb913ec0fec8ab43af4f253
Florian Ohrel <ohrel.florian@pm.me>
2025-03-21 03:35:49 +0100

    feat: add utility functions for formatting durations in HH:MM:SS and verbose formats

--- packages/server/src/common/utils/time.ts
+++ packages/server/src/common/utils/time.ts
@@ -18,3 +18,20 @@ export function format_HHMMSS(duration?: number): string {
     `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   );
 }
+
+export function format_HHMM_verbose(duration?: number): string {
+  if (!duration) return '';
+
+  let seconds = Math.floor(duration / 1000);
+  let minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  minutes %= 60;
+  seconds %= 60;
+
+  return (
+    (hours > 0 ? hours + ' hr ' : '') +
+    (minutes > 0 ? minutes + ' min ' : '') +
+    (seconds > 0 ? seconds + ' sec' : '')
+  );
+}
